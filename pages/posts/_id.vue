6dbae22ec8a3e1d2ab45f6302165ad4d4c251b1f
Diego Camino <diego.camino.r@gmail.com>
2020-08-09 22:24:29 -0500

    Adding axios and page posts

---
+++ pages/posts/_id.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Post 1</h1>
+</template>
---
+++ pages/posts/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Posts</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        {{ post.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      posts: []
+    }
+  },
+  async created () {
+    try {
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
+      this.posts = res.data
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+</script>
