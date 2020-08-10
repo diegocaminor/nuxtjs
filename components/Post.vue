4a0ed4e51224f886b1eb9404d66a367b69829247
Diego Camino <diego.camino.r@gmail.com>
2020-08-09 22:55:13 -0500

    Adding styles

--- components/Post.vue
+++ components/Post.vue
@@ -1,15 +1,37 @@
 <template>
-  <div>
-    <p>{{ id }}. {{ title }}</p>
-  </div>
+  <nuxt-link :to="'/posts/' + id">
+    <div class="post">
+      <p>{{ id }}. {{ title }}</p>
+    </div>
+  </nuxt-link>
 </template>
 
 <script>
 export default {
   name: 'Post',
   props: {
-    id: { type: String, default: '', required: true },
+    id: { type: Number, required: true },
     title: { type: String, default: '', required: true }
   }
 }
 </script>
+
+<style scoped>
+  .post {
+    padding: 1rem;
+    border: 1px solid whitesmoke;
+    margin: 1rem 0;
+  }
+  .post:hover {
+    background: #2d3436;
+    border: 1px solid #6c5ce7;
+    cursor: pointer;
+  }
+  a {
+    color: whitesmoke;
+    text-decoration: none;
+  }
+  a:hover {
+    color: #6c5ce7;
+  }
+</style>
