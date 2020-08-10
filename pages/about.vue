f39aba4721e772cf4dc64c2f861aa00e84fbdada
Diego Camino <diego.camino.r@gmail.com>
2020-08-09 21:19:49 -0500

    Add routes

---
+++ pages/about.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>About</h1>
+    <p>
+      Excepteur consequat velit cillum dolor esse. Nulla aliquip cupidatat commodo minim labore ipsum velit consequat incididunt in laborum proident. Deserunt adipisicing consectetur minim eiusmod deserunt labore cillum quis irure. Ad id sunt sit irure aute ullamco exercitation. Ea nostrud labore pariatur sit pariatur Lorem elit duis nulla ut.
+      Nulla ullamco consectetur id id exercitation mollit irure incididunt non labore. Et velit sunt pariatur enim cillum ad velit voluptate incididunt exercitation ex laborum. Exercitation deserunt anim cupidatat amet adipisicing qui ea quis sint.
+      Consequat tempor dolor minim esse id incididunt esse. Enim proident mollit sunt exercitation aliquip. Veniam officia proident id elit duis anim ut duis laborum dolore irure. Anim ipsum mollit veniam laboris elit nulla exercitation do dolor. Non laborum laborum Lorem Lorem.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  head () {
+    return {
+      title: 'Nuxt App! | About',
+      meta: [
+        {
+          name: 'description',
+          content: 'Website about posts'
+        }
+      ],
+      htmlAttrs: {
+        lang: 'en'
+      }
+    }
+  }
+}
+</script>
---
+++ pages/team/harold.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Harold</h1>
+</template>
